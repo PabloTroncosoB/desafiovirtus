6301079d2da3c78da39687130030abd55303e420
pablotroncosob <pablotroncosob@gmail.com>
2020-08-16 20:39:22 -0400

    v1

---
+++ js/elementos.js
@@ -0,0 +1,27 @@
+const frentes=[
+    {
+        "nombre":"Estrategia Digital",
+        "texto":"Encontramos la intersección de las necesidades de los usuarios y la propuesta de valor de nuestros clientes, para luego innovar y alcanzar las metas",
+        "gliph":"torre"
+    },
+    {
+        "nombre":"Diseño de experiencia del cliente",
+        "texto":"A través del lenguaje de diseño, creamos experiencias consistentes y eficaces que trabajan para las personas a las que servimos",
+        "gliph":"cabeza"
+    },
+    {
+        "nombre":"Estrategia Digital",
+        "texto":"Gestionamos la demanda en base a los requisitos del pipeline del negocio para asegurar una entrega constante que se prioriza de manera integral",
+        "gliph":"digital"
+    },
+    {
+        "nombre":"Ejecución Digital",
+        "texto":"Convertimos los conceptos en realidad a través de un prototipeo ágil y desarrollo de MVP para transformar e innovar con velocidad",
+        "gliph":"agil"
+    },
+    {
+        "nombre":"Digital O&M",
+        "texto":"Ofrecemos a nuestros clientes desarrollo y mejora continua a través del análisis de data y el feedback del cliente",
+        "gliph":"engranaje"
+    }
+];
